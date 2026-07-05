7521b7e562748af9688696d6d9a5f82720382d62
Mehmet Ali <153413232+mehmetalidsy@users.noreply.github.com>
2026-07-05 12:44:04 +0300

    fix: update build configurations for Android and iOS; adjust version to 0.1.6+7; add ProGuard rules

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystorePropertiesFile = rootProject.file("kutra_key.jks")
 val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
@@ -37,19 +37,26 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
         }
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,14 @@
+-keep class io.flutter.app.** { *; }
+-keep class io.flutter.plugin.**  { *; }
+-keep class io.flutter.util.**  { *; }
+-keep class io.flutter.view.**  { *; }
+-keep class io.flutter.embedding.** { *; }
+
+-keep class androidx.security.crypto.** { *; }
+-keep class com.google.android.gms.auth.api.phone.** { *; }
+
+-keep class com.google.zxing.** { *; }
+-keep class com.budius.** { *; }
+
+-keepattributes *Annotation*
+-keepclassmembers class * implements java.io.Serializable
