a5744563574cb81d4cd4de285952b8452d8fad06
Mehmet Ali <153413232+mehmetalidsy@users.noreply.github.com>
2026-07-05 15:25:30 +0300

    fix: update version to 0.1.13+13 in pubspec.yaml; adjust signing config handling in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,9 +35,9 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            if (keystorePropertiesFile.exists()) {
+    if (keystorePropertiesFile.exists()) {
+        signingConfigs {
+            create("release") {
                 storeFile = file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
@@ -48,7 +48,9 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            }
 
             isMinifyEnabled = true
             isShrinkResources = true
