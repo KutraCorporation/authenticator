4917b5afdfb9066f547982eec25c6e5af5cf44d4
Mehmet Ali <153413232+mehmetalidsy@users.noreply.github.com>
2026-07-05 13:36:47 +0300

    fix: update Android build configuration; set compileSdk and ndkVersion explicitly; bump version to 0.1.10+10 in pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.kutra.authenticator"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -29,8 +29,8 @@ android {
 
     defaultConfig {
         applicationId = "com.kutra.authenticator"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 26
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
